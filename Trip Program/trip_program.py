ca05db68aef34ff7d6038dc9e5f4bff898cc4d77
CSA-RC <42678936+CSA-RC@users.noreply.github.com>
2018-10-25 11:19:17 -0500

    Update Trip Program/trip_program.py

--- Trip Program/trip_program.py
+++ Trip Program/trip_program.py
@@ -1,5 +1,5 @@
 """
-    trip_program version 1.1 allows users to make a log of their trips
+    trip_program version 1.2.1 allows users to make a log of their trips
     Copyright (C) 2018  Ryan I Callahan
 
     This program is free software: you can redistribute it and/or modify
@@ -19,7 +19,12 @@
 from tkinter import *
 from tkinter import ttk
 
+root = Tk()
 
+for x in range(0, 6):
+    root.columnconfigure(x, weight=1)
+for x in range(0, 6):
+    root.rowconfigure(x, weight=1)
 #Clears all selections, sets the method of transportation back to "Other"
 def clear():
     choicebox.set("Other")
@@ -57,9 +62,9 @@ def exit():
 #The about window
 def about():
     abt = Toplevel()
-    abt.geometry('180x130')
+    abt.geometry('180x150')
     abt.title("About")
-    abtmsg = Message(abt, text="This program is an example of a Travel log, used to input information about the user's trips around the world.")
+    abtmsg = Message(abt, text="Made by Ryan Callahan\n\nThis program is an example of a Travel log, used to input information about the user's trips around the world.\n\nVersion 1.2.1")
     abtmsg.pack()
     close = Button(abt, text="Close", command=abt.destroy)
     close.pack()
@@ -76,7 +81,6 @@ def license():
     close.pack()
 
 
-root = Tk()
 menubar = Menu(root)
 filemenu = Menu(menubar, tearoff=0)
 filemenu.add_command(label="Save", command=save)
@@ -90,75 +94,69 @@ menubar.add_cascade(label="Help", menu=helpmenu)
 
 root.config(menu=menubar)
 
-
-#Frames are used to put all buttons and menus in the correct position
-mframe=Frame()
-mframe.grid(column=0, row=1)
-
-
-lframe=Frame()
-lframe.grid(column=0, row=0)
-
-
+lframe = Frame(root)
+lframe.grid(column=1, row=2)
 #Aesthetic title on program
-label = Label(lframe, text="Travel Log\n----------")
-label.grid(column=0, row=0)
+label = Label(root, text="Travel Log\n----------")
+label.grid(column=1, row=0, columnspan=2, sticky='e')
 
 
 #Month selection
-slab=Label(mframe, text="Month:  ")
-slab.grid(column=0, row=2, sticky='e')
+slab=Label(root, text="Month:  ")
+slab.grid(column=0, row=3, sticky='e')
 spinval = StringVar()
-sp = Spinbox(mframe,state='readonly', wrap=True, width=28, values=("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"))
-sp.grid(column=1, row=2, sticky='w')
+sp = Spinbox(root, state='readonly', wrap=True, width=28, values=("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"))
+sp.grid(column=1, row=3, sticky='w')
 
 
 #Method of travel selection
 tvar = StringVar()
-tlab=Label(mframe, text="Transportation:  ")
-tlab.grid(column=0, row=0, sticky='e')
+tlab=Label(root, text="Transportation:  ")
+tlab.grid(column=0, row=1, sticky='e')
 travel = ["Airplane", "Train", "Car", "Boat", "Other"]
-choicebox = ttk.Combobox(mframe, width=27, state='readonly', value=travel)
-choicebox.grid(column=1, row=0, sticky='w')
+choicebox = ttk.Combobox(root, width=27, state='readonly', value=travel)
+choicebox.grid(column=1, row=1, sticky='w')
 choicebox.bind('<<ComboboxSelected>>', cbox)
 choicebox.set("Other")
 
 
 #Notes selection
-notebox = Text(mframe, width=20, height=10, wrap='word')
-notebox.grid(column=4, row=1, sticky='w')
+notebox = Text(root, width=20, height=10, wrap='word')
+notebox.grid(column=4, row=2, sticky='w')
 
 
 #Country selection
-clab=Label(mframe, text="Countries:  ")
-clab.grid(column=0, row=1, sticky='e')
+clab=Label(root, text="Countries:  ")
+clab.grid(column=0, row=2, sticky='e')
 countries = ["Argentina", "Australia", "The Bahamas", "Belgium", "Brazil", "Czech Republic", "France", "Germany", "Ireland", "Italy", "Russia", "Singapore", "Sweden", "United Kingdom", "United States", "Vietnam", "Other"]
-itembox = Listbox(mframe, width=30, height=10, listvariable=countries, selectmode=SINGLE)
+itembox = Listbox(lframe, width=28, height=10, listvariable=countries, selectmode=SINGLE)
 for item in countries:
     itembox.insert('end', '%s' % item)
-itembox.grid(column=1, row=1, sticky='w')
+itembox.grid(column=1, row=2, sticky='w')
 itembox.bind('<FocusIn>')
 
 
 #Labels used as spacers
-l1 = Label(mframe, text="   Notes:  ")
-l1.grid(column=3, row=1, sticky='e')
-l2 = Label(mframe, text="")
-l2.grid(row=3, column=1)
-l3 = Label(mframe, text=" ")
-l3.grid(row=3, column=5)
+l1 = Label(root, text="   Notes:  ")
+l1.grid(column=3, row=2, sticky='e')
+l2 = Label(root, text="")
+l2.grid(row=4, column=1)
+l3 = Label(root, text=" ")
+l3.grid(row=4, column=5)
+
+size = ttk.Sizegrip(root)
+size.grid(row=6, column=6, sticky='se')
+
+calculate = Button(root, text="SUBMIT", command=save, width=25)
+calculate.grid(column=1, row=5)
 
 
-calculate = Button(mframe, text="SUBMIT", command=save, width=25)
-calculate.grid(column=1, row=4)
+clearb = Button(root, text="CLEAR", command=clear, width=10)
+clearb.grid(column=3, row=5, sticky='w', columnspan=2)
 
 
-clearb = Button(mframe, text="CLEAR", command=clear, width=10)
-clearb.grid(column=3, row=4, sticky='w', columnspan=2)
-
-
-s = ttk.Scrollbar(mframe, orient=VERTICAL, command=itembox.yview)
-s.grid(column=2, row=1, sticky='nse')
+s = ttk.Scrollbar(lframe, orient=VERTICAL, command=itembox.yview)
+s.grid(column=2, row=2, sticky='nse')
 itembox.configure(yscrollcommand=s.set)
 
 
